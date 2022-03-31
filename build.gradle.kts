767108a5da24b6365c98d5d9faf8173f9da998d3
Simon Jahreiss <simon.jahreiss@linked-planet.com>
2022-03-31 17:24:22 +0200

    Added license headers to all files and the corresponding gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,31 @@
 plugins {
     kotlin("js") version "1.5.31" apply false
     kotlin("multiplatform") version "1.5.31" apply false
+    id("com.github.hierynomus.license") version "0.16.1"
+}
+
+subprojects {
+    apply(plugin = "com.github.hierynomus.license")
+    license {
+        header = rootProject.file("LICENSE-HEADER.txt")
+        strictCheck = true
+
+        exclude("**/*.properties")
+        exclude("**/*.json")
+
+        ext["year"] = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)
+        ext["owner"] = "linked-planet GmbH"
+        ext["email"] = "info@linked-planet.com"
+    }
+
+    tasks.register<com.hierynomus.gradle.license.tasks.LicenseCheck>("licenseCheckInSrc") {
+        source = fileTree(project.projectDir).matching { include("src/**/*") }
+    }
+    tasks.register<com.hierynomus.gradle.license.tasks.LicenseFormat>("licenseFormatInSrc") {
+        source = fileTree(project.projectDir).matching { include("src/**/*") }
+    }
+    tasks["license"].dependsOn("licenseCheckInSrc")
+    tasks["licenseFormat"].dependsOn("licenseFormatInSrc")
 }
 
 group = "com.linked-planet"
